2bce383f2ea6db36ffb4db269f3042f6b54892c1
suijumoku <annryuuki@gmail.com>
2024-10-16 14:16:03 +0900

    サイドミラーを追加しました

--- src/Assets/Scripts/PlayerController.cs
+++ src/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,33 @@
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float FORWARD_ACCRLERATION = 30.0f; //加速
+    [SerializeField] float SIDE_ACCELERATION = 100.0f;
+    [SerializeField] float FORWARD_DAMPING = 4.0f; // 自動減速
+    [SerializeField] float SIDE_DAMPING = 4.0f;
+
+    float forward = 0.0f;
+    float side = 0.0f;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        forward += z * FORWARD_ACCRLERATION * Time.deltaTime;
+        side += x * SIDE_ACCELERATION * Time.deltaTime;
+
+        // 移動
+        transform.Translate(Vector3.forward * forward * Time.deltaTime);
+        transform.Rotate(Vector3.up, side * forward * Time.deltaTime);
+
+        // 自動減速
+        forward -= forward * FORWARD_DAMPING * Time.deltaTime;
+        side -= side * SIDE_DAMPING * Time.deltaTime;
     }
 }
